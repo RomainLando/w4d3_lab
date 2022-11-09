f286edc9ad3bc8fc36d4473f5f0238e9513604b5
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-09 12:04:49 +0000

    added delete cascade

--- db/music_library.sql
+++ db/music_library.sql
@@ -10,7 +10,7 @@ CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     genre VARCHAR(255),
-    artist_id INT REFERENCES artists(id)
+    artist_id INT REFERENCES artists(id) ON DELETE CASCADE
 );
 
 INSERT INTO artists (full_name) 
